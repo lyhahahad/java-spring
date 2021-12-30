2cc98b3e9ea89b1b021ce57ed4635f707d1ada9f
lyhahahad <96465753+lyhahahad@users.noreply.github.com>
2021-12-30 11:30:44 +0900

    Add files via upload

---
+++ java/networking/netwokring/netwokring/src/main/java/com/example/netwokring/NetwokringApplication.java
@@ -0,0 +1,13 @@
+package com.example.netwokring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetwokringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetwokringApplication.class, args);
+	}
+
+}
