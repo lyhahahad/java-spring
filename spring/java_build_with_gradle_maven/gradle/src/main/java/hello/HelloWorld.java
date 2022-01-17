99bee3ac43d13b0a0c9aaddb117214f923bbdf45
lyhahahad <96465753+lyhahahad@users.noreply.github.com>
2022-01-17 10:59:28 +0900

    Add files via upload

--- spring/java_build_with_gradle_maven/gradle/src/main/java/hello/HelloWorld.java
+++ spring/java_build_with_gradle_maven/gradle/src/main/java/hello/HelloWorld.java
@@ -1,7 +1,10 @@
 package main.java.hello;
 
+
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("The current local time is: ");
+
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
